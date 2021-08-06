6559ecc2a72d55972e7c7e76b0b7f3c1b4dfa515
Matheus Lopes Marques <2000ttu@gmail.com>
2021-08-05 23:40:45 -0300

    Add funcion (save verses on device storage when open)

--- src/components/VersesList/index.js
+++ src/components/VersesList/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar, View, Text } from 'react-native';
 import Axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import {
-  VerseList, VerseBox, VerseText, VerseNumber
-} from './styles';
+import { VerseList, VerseBox, VerseText, VerseNumber } from './styles';
 
 const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ik1vbiBBdWcgMDIgMjAyMSAxNjowMzozMyBHTVQrMDAwMC4yMDAwdHR1QGdtYWlsLmNvbSIsImlhdCI6MTYyNzkyMDIxM30.lfeWPXO6QmAaze-ArjWt6GyGJSyoX4Vn022S7zqNnU0";
 
@@ -20,13 +19,28 @@ const Verse = ({route, navigation, number, text}) => (
 function VersesList({ route, navigation }){
   const { name, abbrev, chapter } = route.params;
   const [Verses, setVerses] = useState([]);
+
+  const getData = async () => {
+    try{
+      const result = await AsyncStorage.getItem(`@${abbrev}-${chapter}`)
+      if( !result ){
+        Axios({
+          method: "GET",
+          url: `https://www.abibliadigital.com.br/api/verses/nvi/${abbrev}/${chapter}`,
+          headers: { Authorization: TOKEN }
+        })
+        .then( ({data}) => {
+          setVerses(data.verses)
+          AsyncStorage.setItem(`@${abbrev}-${chapter}`, JSON.stringify(data.verses));
+        } )
+      }else{
+        setVerses(JSON.parse(result))
+      }
+    }catch(e){ console.log(e) }
+  }
+
   useEffect( () => {
-    Axios({
-      method: "GET",
-      url: `https://www.abibliadigital.com.br/api/verses/nvi/${abbrev}/${chapter}`,
-      headers: { Authorization: TOKEN }
-    })
-    .then( ({data}) => setVerses(data.verses) )
+    getData();
   }, [] )
   return(
     <VerseList
